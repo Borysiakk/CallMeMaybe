2d20972f4925ee1d60fc1e437404fa80058b3018
Borysiak <szymaborys@gmail.com>
2020-12-28 11:47:15 +0100

    Add Web

---
+++ CallMeMaybe.Web/Startup.cs
@@ -0,0 +1,61 @@
+
+using CallMeMaybe.Infrastructure;
+using CallMeMaybe.Persistence;
+using CallMeMaybe.WebApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CallMeMaybe.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IConfiguration configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebApi();
+            services.AddPersistence();
+            services.AddInfrastructure(configuration);
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseWebApi(env);
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+        
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
